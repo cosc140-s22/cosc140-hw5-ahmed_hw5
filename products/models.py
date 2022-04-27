0bb7281b5d0ee97da614b6d7002b6dff0c91e104
Ahmed Kamran <akamran@colgate.edu>
2022-04-27 01:13:49 +0100

    Allow zero or more photos per product

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import QuerySet
 
 class Product(models.Model):
     name = models.CharField(max_length=50, blank=False)
@@ -17,3 +18,18 @@ class Product(models.Model):
             return f"Age {self.minimum_age_appropriate}"
         else:
             return f"Ages {self.minimum_age_appropriate} to {self.maximum_age_appropriate}"
+    
+    def random_image(self):
+        img_query:QuerySet = self.productimage_set.all()
+        if(img_query.exists()):
+            return img_query.order_by("?")[0]
+        else:
+            return None
+
+class ProductImage(models.Model):
+    image = models.ImageField(upload_to='product_images/')
+    caption = models.CharField(max_length=100, blank=False)
+    product = models.ForeignKey(to=Product, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.caption
--- products/views.py
+++ products/views.py
@@ -1,20 +1,31 @@
+from genericpath import exists
+from typing import List
 from django.shortcuts import render, get_object_or_404
-from django.db.models import Prefetch
+from django.db.models import Prefetch, QuerySet
 from .models import Product
 from .forms import ProductFilterForm
 
 def index(request):
-    products = Product.objects.all().order_by('name')
+    products:List[Product] = Product.objects.all().order_by('name')
     form = ProductFilterForm(request.GET)
     name_search = request.GET.get('name_search')
+    
     if name_search:
         products = products.filter(name__icontains=name_search)
+    
+    Product.objects.prefetch_related(Prefetch('productimage_set'))
 
-    context = {'products': products, 'form': form}
+    product_images = [p.random_image() for p in products]
+    
+    context = {'products': zip(products, product_images), 'form': form}
+    
     return render(request, 'products/index.html', context)
 
 def show(request, product_id):
     p = get_object_or_404(Product, pk=product_id)
-    context = { 'product':p }
+
+    images:QuerySet = p.productimage_set.all()
+
+    context = { 'product':p, 'images': images if images.exists() else None }
     return render(request, 'products/show.html', context)
     
