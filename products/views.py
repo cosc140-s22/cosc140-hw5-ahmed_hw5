859e417d638488b56ffc621f4c5276394e04d9d8
Ahmed Kamran <akamran@colgate.edu>
2022-04-27 04:47:19 +0100

    sorting implemented

--- products/views.py
+++ products/views.py
@@ -1,12 +1,16 @@
 from genericpath import exists
 from typing import List
+from django.http import HttpRequest
 from django.shortcuts import render, get_object_or_404
 from django.db.models import Prefetch, QuerySet
 from .models import Product
 from .forms import ProductFilterForm
 
-def index(request):
-    products:List[Product] = Product.objects.all().order_by('name')
+def index(request:HttpRequest):
+    get_params = request.GET
+    order_index = get_params.get('sort','name')
+    order_index = 'minimum_age_appropriate' if order_index == 'age' else order_index
+    products:List[Product] = Product.objects.all().order_by(order_index)
     form = ProductFilterForm(request.GET)
     name_search = request.GET.get('name_search')
     
